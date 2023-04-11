74d937789904c40ce9688442022108964bfc6e52
“IsaKaillany” <isakaillany123@gmail.com>
2023-04-10 23:02:45 -0300

    att navBar

--- receita_2/lib/main.dart
+++ receita_2/lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class CustomNavBar extends StatelessWidget { //transforma a class em um Widget a partir da já existente StatelessWidget (herança)
-    CustomNavBar();
+    List<Icon> icons;
+    CustomNavBar({this.icons = const []});
 
     void botaoFoiTocado(int index) {
         print("Tocaram no botão $index");
@@ -9,20 +10,10 @@ class CustomNavBar extends StatelessWidget { //transforma a class em um Widget a
 
     @override
     Widget build(BuildContext context) {
-        return BottomNavigationBar(onTap: botaoFoiTocado, items: const [
-            BottomNavigationBarItem(
-                label: "Cafés",
-                icon: Icon(Icons.coffee_outlined)
-            ),
-            BottomNavigationBarItem(
-                label: "Cervejas",
-                icon: Icon(Icons.local_drink_outlined)
-            ),
-            BottomNavigationBarItem(
-                label: "Nações",
-                icon: Icon(Icons.flag_outlined)
-            )
-        ]);
+        return BottomNavigationBar(
+            onTap: botaoFoiTocado, 
+            items: icons.map((obj) => BottomNavigationBarItem(icon: obj, label: "Label")
+        ).toList());
     }
 }
 
@@ -69,7 +60,7 @@ class MyApp extends StatelessWidget {
             home: Scaffold(
                 appBar: CustomAppBar(),
                 body: CustomBody(),
-                bottomNavigationBar: CustomNavBar()),
+                bottomNavigationBar: CustomNavBar(icons: [Icon(Icons.coffee_outlined), Icon(Icons.local_drink_outlined), Icon(Icons.flag_outlined)])),
         );
     }   
 }

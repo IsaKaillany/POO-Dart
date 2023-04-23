f3a2931f2abd19a999ab520341cbd1057cc3aa86
IsaKaillany <isakaillany123@gmail.com>
2023-04-22 23:22:58 -0300

    add forms

--- tarefa_6/lib/main.dart
+++ tarefa_6/lib/main.dart
@@ -1,125 +1,161 @@
-import 'package:flutter/material.dart';
+import "package:flutter/material.dart";
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        theme: ThemeData(primarySwatch: Colors.teal),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            appBar: AppBar(
+                title: Text("Forms"),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+            body: MyForm(),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class MyForm extends StatefulWidget {
+    @override
+    _MyFormState createState() => _MyFormState();
+}
+
+class _MyFormState extends State<MyForm> {
+    final _formKey = GlobalKey<FormState>();
+    String _age = ""; 
+    String _education = "sup";
+
+    @override
+    Widget build(BuildContext context) {
+        return Container(
+            padding: EdgeInsets.all(20),
+            child: Form(
+                key: _formKey,
+                child: Column(
+                    children: <Widget>[
+                        TextFormField(
+                            decoration: InputDecoration(
+                                labelText: "Nome",
+                            ),
+                            validator: (value) {
+                                if (value!.isEmpty) {
+                                    return "Por favor, digite seu nome";
+                                }
+                                return null;
+                            },
+                        ),
+                        TextFormField(
+                            keyboardType: TextInputType.emailAddress,
+                            decoration: InputDecoration(
+                                labelText: "E-mail",
+                            ),
+                            validator: (value) {
+                                if (value!.isEmpty) {
+                                    return "Por favor, digite seu e-mail";
+                                }
+                                return null;
+                            },
+                        ),
+                        TextFormField(
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                                labelText: "Telefone",
+                            ),
+                            validator: (value) {
+                                if (value!.isEmpty) {
+                                    return "Por favor, digite seu telefone";
+                                }
+                                return null;
+                            },
+                        ),
+                        SizedBox(height: 15),
+                        Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                                Text(
+                                    "Idade:",
+                                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                                ),
+                                RadioListTile<String>(
+                                    title: Text("0 a 18 anos"),
+                                    value: "0-18",
+                                    groupValue: _age,
+                                    onChanged: (value) {
+                                        setState(() {
+                                            _age = value!;
+                                        });
+                                    },
+                                ),
+                                RadioListTile<String>(
+                                    title: Text("19 a 45 anos"),
+                                    value: "19-40",
+                                    groupValue: _age,
+                                    onChanged: (value) {
+                                        setState(() {
+                                            _age = value!;
+                                        });
+                                    },
+                                ),
+                                RadioListTile<String>(
+                                    title: Text("+ de 45 anos"),
+                                    value: "+45",
+                                    groupValue: _age,
+                                    onChanged: (value) {
+                                        setState(() {
+                                            _age = value!;
+                                        });
+                                    },
+                                ),
+                                SizedBox(height: 15),
+                                Text(
+                                    "Nível de escolaridade:",
+                                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                                ),
+                                DropdownButtonFormField<String>(
+                                    value: _education,
+                                    items: [
+                                        DropdownMenuItem(
+                                            child: Text("Superior completo/incompleto"),
+                                            value: "sup",
+                                        ),
+                                        DropdownMenuItem(
+                                            child: Text("Ensino médio completo/incompleto"),
+                                            value: "em",
+                                        ),
+                                        DropdownMenuItem(
+                                            child: Text("Ensino fundamental completo/incompleto"),
+                                            value: "ef",
+                                        ),
+                                    ],
+                                    onChanged: (value) {
+                                        setState(() {
+                                            _education = value!;
+                                        });
+                                    },
+                                ),
+                            ],
+                        ),
+                        Padding(
+                            padding: const EdgeInsets.symmetric(vertical: 16.0),
+                            child: ElevatedButton(
+                                onPressed: () {
+                                    if (_formKey.currentState!.validate()) {
+                                        ScaffoldMessenger.of(context).showSnackBar(
+                                            SnackBar(
+                                                content: Text("Formulário em processamento"),
+                                            ),
+                                        );
+                                    }
+                                },
+                                child: Text("Enviar"),
+                            ),
+                        ),
+                    ],
+                ),
+            ),
+        );
+    }
 }

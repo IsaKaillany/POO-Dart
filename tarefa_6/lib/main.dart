f0d1c230260105fbe57da0b8cbbc7cea40330573
IsaKaillany <isakaillany123@gmail.com>
2023-04-23 22:01:08 -0300

    add slider and scroll

--- tarefa_6/lib/main.dart
+++ tarefa_6/lib/main.dart
@@ -25,125 +25,138 @@ class MyForm extends StatefulWidget {
 
 class _MyFormState extends State<MyForm> {
     final _formKey = GlobalKey<FormState>();
-    String _age = ""; 
+    String _nacionality = ""; 
     String _education = "sup";
+    double _currentSlideValue = 1;
 
     @override
     Widget build(BuildContext context) {
-        return Container(
-            padding: EdgeInsets.all(20),
-            child: Form(
-                key: _formKey,
-                child: Column(
-                    children: <Widget>[
-                        TextFormField(
-                            decoration: InputDecoration(
-                                labelText: "Nome",
-                            ),
-                            validator: (value) {
-                                if (value!.isEmpty) {
-                                    return "Por favor, digite seu nome";
-                                }
-                                return null;
-                            },
-                        ),
-                        TextFormField(
-                            keyboardType: TextInputType.emailAddress,
-                            decoration: InputDecoration(
-                                labelText: "E-mail",
-                            ),
-                            validator: (value) {
-                                if (value!.isEmpty) {
-                                    return "Por favor, digite seu e-mail";
-                                }
-                                return null;
-                            },
-                        ),
-                        SizedBox(height: 15),
-                        Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
+        return SingleChildScrollView( //configura o tipo de scroll 
+            scrollDirection: Axis.vertical,
+            child: 
+                Container(
+                    padding: EdgeInsets.all(15),
+                    child: Form(
+                        key: _formKey,
+                        child: Column(
                             children: <Widget>[
-                                Text(
-                                    "Idade:",
-                                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                                ),
-                                RadioListTile<String>(
-                                    title: Text("0 a 18 anos"),
-                                    value: "0-18",
-                                    groupValue: _age,
-                                    onChanged: (value) {
-                                        setState(() {
-                                            _age = value!;
-                                        });
+                                TextFormField(
+                                    decoration: InputDecoration(
+                                        labelText: "Nome",
+                                    ),
+                                    validator: (value) {
+                                        if (value!.isEmpty) {
+                                            return "Por favor, digite seu nome";
+                                        }
+                                        return null;
                                     },
                                 ),
-                                RadioListTile<String>(
-                                    title: Text("19 a 45 anos"),
-                                    value: "19-40",
-                                    groupValue: _age,
-                                    onChanged: (value) {
-                                        setState(() {
-                                            _age = value!;
-                                        });
+                                TextFormField(
+                                    keyboardType: TextInputType.emailAddress,
+                                    decoration: InputDecoration(
+                                        labelText: "E-mail",
+                                    ),
+                                    validator: (value) {
+                                        if (value!.isEmpty) {
+                                            return "Por favor, digite seu e-mail";
+                                        }
+                                        return null;
                                     },
-                                ),
-                                RadioListTile<String>(
-                                    title: Text("+ de 45 anos"),
-                                    value: "+45",
-                                    groupValue: _age,
-                                    onChanged: (value) {
-                                        setState(() {
-                                            _age = value!;
-                                        });
-                                    },
-                                ),
+                                ),                                
                                 SizedBox(height: 15),
-                                Text(
-                                    "Nível de escolaridade:",
-                                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                                ),
-                                DropdownButtonFormField<String>(
-                                    value: _education,
-                                    items: [
-                                        DropdownMenuItem(
-                                            child: Text("Superior completo/incompleto"),
-                                            value: "sup",
+                                Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: <Widget>[
+                                        Text(
+                                            "Idade:",
+                                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
                                         ),
-                                        DropdownMenuItem(
-                                            child: Text("Ensino médio completo/incompleto"),
-                                            value: "em",
+                                        Slider(
+                                            value: _currentSlideValue, 
+                                            label: _currentSlideValue.round().toString(),
+                                            max: 100,
+                                            divisions: 100,
+                                            onChanged: ((value) {
+                                                setState(() {
+                                                    _currentSlideValue = value;
+                                                });
+                                            })
                                         ),
-                                        DropdownMenuItem(
-                                            child: Text("Ensino fundamental completo/incompleto"),
-                                            value: "ef",
+                                        Text(
+                                            "Nacionalidade:",
+                                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                                        ),
+                                        RadioListTile<String>(
+                                            title: Text("Brasileira"),
+                                            contentPadding: EdgeInsetsDirectional.zero,
+                                            value: "br",
+                                            groupValue: _nacionality,
+                                            onChanged: (value) {
+                                                setState(() {
+                                                    _nacionality = value!;
+                                                });
+                                            },
+                                        ),
+                                        RadioListTile<String>(
+                                            title: Text("Estrangeiro"),
+                                            contentPadding: EdgeInsetsDirectional.zero,
+                                            value: "es",
+                                            groupValue: _nacionality,
+                                            onChanged: (value) {
+                                                setState(() {
+                                                    _nacionality = value!;
+                                                });
+                                            },
+                                        ),
+                                        SizedBox(height: 15),
+                                        Text(
+                                            "Nível de escolaridade:",
+                                            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                                        ),
+                                        DropdownButtonFormField<String>(
+                                            value: _education,
+                                            items: [
+                                                DropdownMenuItem(
+                                                    child: Text("Superior completo/incompleto"),
+                                                    value: "sup",
+                                                ),
+                                                DropdownMenuItem(
+                                                    child: Text("Ensino médio completo/incompleto"),
+                                                    value: "em",
+                                                ),
+                                                DropdownMenuItem(
+                                                    child: Text("Ensino fundamental completo/incompleto"),
+                                                    value: "ef",
+                                                ),
+                                            ],
+                                            onChanged: (value) {
+                                                setState(() {
+                                                    _education = value!;
+                                                });
+                                            },
                                         ),
                                     ],
-                                    onChanged: (value) {
-                                        setState(() {
-                                            _education = value!;
-                                        });
-                                    },
+                                ),
+                                Padding(
+                                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                                    child: ElevatedButton(
+                                        onPressed: () {
+                                            if (_formKey.currentState!.validate()) {
+                                                ScaffoldMessenger.of(context).showSnackBar(
+                                                    SnackBar(
+                                                        backgroundColor: Colors.teal,
+                                                        content: Text("Formulário em processamento"),
+                                                    ),
+                                                );
+                                            }
+                                        },
+                                        child: Text("Enviar"),
+                                    ),
                                 ),
                             ],
                         ),
-                        Padding(
-                            padding: const EdgeInsets.symmetric(vertical: 16.0),
-                            child: ElevatedButton(
-                                onPressed: () {
-                                    if (_formKey.currentState!.validate()) {
-                                        ScaffoldMessenger.of(context).showSnackBar(
-                                            SnackBar(
-                                                content: Text("Formulário em processamento"),
-                                            ),
-                                        );
-                                    }
-                                },
-                                child: Text("Enviar"),
-                            ),
-                        ),
-                    ],
-                ),
-            ),
+                    ),
+                )
         );
     }
 }
